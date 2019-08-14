656f50d169beb8b473c5fa4f8284a388b3d8385c
David McGarvey <davidjmcgarvey@gmail.com>
2019-08-14 15:05:16 -0700

    text field for 'other' progress

--- js/script.js
+++ js/script.js
@@ -9,10 +9,15 @@ Full Stack JavaScript Techdegree Project 3
 $('#name').focus();
 
 // Create text area to be displayed if 'other' option selected
-const $otherInput = $('<input type="text" name="user-title" id="user-title" style="display:none">');
-const $otherOption = $('option[value="other"]');
-$otherOption.on('select', function() {
-  alert('nailed it');
+const $otherInput = $('<input type="text" name="user-title" id="user-title" style="display:block">');
+const $select = $('#title');
+const $otherOption = $('#title option[value="other"]');
+$select.on('change', function(e) {
+  if (e.target.value === 'other') {
+    console.log('nailed it again dave!')
+    $select.append($otherInput);
+    $otherInput.attr('style', 'display=block')
+  };
 });
 
 // Author: David J McGarvey
