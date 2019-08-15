bfc1ba08564c10f144d2daf665a1920e33312d6a
David McGarvey <davidjmcgarvey@gmail.com>
2019-08-15 16:37:36 -0700

    add "Payment Info" section

--- js/script.js
+++ js/script.js
@@ -11,7 +11,6 @@ $('#name').focus();
 // "Job Role" Section --> display text area if 'other' option selected
 $('#title').on('change', function() {
   if (this.value === 'other') {
-    console.log('nailed it again davey!')
     $('label[for="other-title"]').attr('style', 'display:block');
     $('#other-title').attr('style', 'display:block');
   } else {
@@ -44,7 +43,6 @@ $('#design').on('change', function() {
     $('option[value="tomato"]').hide();
     $('option[value="steelblue"]').hide();
     $('option[value="dimgrey"]').hide();
-    console.log('js puns test');
   } else if (this.value === 'heart js') {
     $('option[value="tomato"]').show();
     $('option[value="steelblue"]').show();
@@ -52,7 +50,6 @@ $('#design').on('change', function() {
     $('option[value="cornflowerblue"]').hide();
     $('option[value="darkslategrey"]').hide();
     $('option[value="gold"]').hide();
-    console.log('heart js test');
   } else {
     $('option[value="cornflowerblue"]').hide();
     $('option[value="darkslategrey"]').hide();
@@ -63,12 +60,11 @@ $('#design').on('change', function() {
   }
 });
 
-// "Register" Section -->
-// TODO: Clean this s##t up too! Least DRY code ever!
+// "Register" Section --> prevents user from overlapping activities
+// TODO: Clean this s##t up too! Least DRY code ever! AAAHHH!!!
 
 $('input[name="all"]').on('change', function() {
   if ($(this).is(':checked')) {
-    console.log('checked?');
     $('input[name="js-frameworks"]').attr('disabled', true);
     $('input[name="js-libs"]').attr('disabled', true);
     $('input[name="express"]').attr('disabled', true);
@@ -76,7 +72,6 @@ $('input[name="all"]').on('change', function() {
     $('input[name="build-tools"]').attr('disabled', true);
     $('input[name="npm"]').attr('disabled', true);
   } else {
-    console.log('not checked?');
     $('input[name="js-frameworks"]').attr('disabled', false);
     $('input[name="js-libs"]').attr('disabled', false);
     $('input[name="express"]').attr('disabled', false);
@@ -85,43 +80,65 @@ $('input[name="all"]').on('change', function() {
     $('input[name="npm"]').attr('disabled', false);
   }
 });
-$('input[type="checkbox"]').on('change', function(){
+$('input[type="checkbox"]').on('change', function() {
   if ($(this).attr('name') !== 'all' && $(this).is(':checked')) {
-    console.log('look at you davey!');
     if ($(this).attr('data-day-and-time') === 'Tuesday 1pm-4pm') {
       $('input[data-day-and-time="Tuesday 1pm-4pm"]').not(this).attr('disabled', true);
       $('input[name="all"]').attr('disabled', true);
     }
   } else if ($(this).attr('name') !== 'all' && $(this).is(':not(:checked)')) {
-    console.log('whoa davey!');
     if ($(this).attr('data-day-and-time') === 'Tuesday 1pm-4pm') {
       $('input[data-day-and-time="Tuesday 1pm-4pm"]').not(this).attr('disabled', false);
       $('input[name="all"]').attr('disabled', false);
     }
   }
   if ($(this).attr('name') !== 'all' && $(this).is(':checked')) {
-    console.log('look at you davey!');
     if ($(this).attr('data-day-and-time') === 'Tuesday 9am-12pm') {
       $('input[data-day-and-time="Tuesday 9am-12pm"]').not(this).attr('disabled', true);
       $('input[name="all"]').attr('disabled', true);
     }
   } else if ($(this).attr('name') !== 'all' && $(this).is(':not(:checked)')) {
-    console.log('whoa davey!');
     if ($(this).attr('data-day-and-time') === 'Tuesday 9am-12pm') {
       $('input[data-day-and-time="Tuesday 9am-12pm"]').not(this).attr('disabled', false);
       $('input[name="all"]').attr('disabled', false);
     }
   }
   if ($(this).attr('name') !== 'all' && $(this).is(':checked')) {
-    console.log('look at you davey!');
     if ($(this).attr('data-day-and-time').includes('Wednesday')) {
       $('input[name="all"]').attr('disabled', true);
     }
   } else if ($(this).attr('name') !== 'all' && $(this).is(':not(:checked)')) {
-    console.log('whoa davey!');
     if ($(this).attr('data-day-and-time').includes('Wednesday')) {
       $('input[name="all"]').attr('disabled', false);
     }
+  }
+});
+
+// "Payment" Section -->
+$('option[value="credit card"]').attr('selected', 'selected');
+const $creditCardDiv = $('.credit-card');
+const $payPalDiv = $('.credit-card').next().hide();
+const $bitCoinDiv = $('.credit-card').next().next().hide();
+
+
+$('#payment').on('change', function() {
+  if (this.value === 'paypal') {
+    // console.log('Where you been davey? Welcome back!');
+    $creditCardDiv.hide();
+    $payPalDiv.show();
+    $bitCoinDiv.hide();
+  } else if (this.value === 'bitcoin') {
+    $creditCardDiv.hide();
+    $payPalDiv.hide();
+    $bitCoinDiv.show();
+  } else  if (this.value === 'credit card') {
+    $creditCardDiv.show();
+    $payPalDiv.hide();
+    $bitCoinDiv.hide();
+  } else {
+    $creditCardDiv.hide();
+    $payPalDiv.hide();
+    $bitCoinDiv.hide();
   }
 });
 
